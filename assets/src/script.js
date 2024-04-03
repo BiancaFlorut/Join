00775808f97bab6907e13e1536e0b8dc4465bb60
Bianca Floruț <150796482+BiancaFlorut@users.noreply.github.com>
2024-04-03 21:35:28 +0200

    function getItem

--- assets/src/script.js
+++ assets/src/script.js
@@ -1,6 +1,17 @@
 const STORAGE_TOKEN = 'A4FWT6VNANE57F3YJWY8RKAO2BLJ7XU20BKY7G0X';
 const STORAGE_URL = ' https://remote-storage.developerakademie.org/item';
 
+/**
+ * The function calls the remote saved data with the key parameter.
+ * 
+ * @param {string} key 
+ * @returns the value of the saved data with the key and token
+ */
+function getItem(key) {
+    const URL = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
+    return fetch(URL);
+}
+
 async function init() {
     await includeHTML(); // hier wird auf includeHTML gewartet, bis es komplett fertig ist, ohne das await werden beide fast zeitgleich verarbeitet und führt zu fehlermeldungen
     document.getElementById('headline').innerHTML = 'Herzlich willkommen!'; // soll die Überschrift im header ändern
