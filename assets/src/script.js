cd527bde425787083b72d2f8d7687134472f33d4
ThomEckert <thec.lernt@gmx.de>
2024-04-04 13:24:21 +0200

    update haeder nav-bar hover-effects on nav rollable content

--- assets/src/script.js
+++ assets/src/script.js
@@ -13,20 +13,6 @@ function getItem(key) {
 }
 
 async function init() {
-    await includeHTML(); // hier wird auf includeHTML gewartet, bis es komplett fertig ist, ohne das await werden beide fast zeitgleich verarbeitet und führt zu fehlermeldungen
-    document.getElementById('headline').innerHTML = 'Herzlich willkommen!'; // soll die Überschrift im header ändern
+    await includeHTML();
 }
 
-async function includeHTML() {
-    let includeElements = document.querySelectorAll('[w3-include-html]');
-    for (let i = 0; i < includeElements.length; i++) {
-        const element = includeElements[i];
-        file = element.getAttribute("w3-include-html"); // "includes/header.html" 
-        let resp = await fetch(file); // fetch holt sich informationen ausm netz
-        if (resp.ok) {
-            element.innerHTML = await resp.text();
-        } else {
-            element.innerHTML = 'Page not found';
-        }
-    }
-}
