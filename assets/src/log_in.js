bd26c7543ec8cb16ea45f464778a0a99f480c76d
Ahmet Uysal <ahmet-uysal@outlokk.de>
2024-04-05 10:31:35 +0200

    login site

---
+++ assets/src/log_in.js
@@ -0,0 +1,14 @@
+function login() {
+    let email = document.getElementById('email');
+    let password = document.getElementById('password');
+    let user = user.find(u => u.email == email.value && u.password == password.value);
+}
+
+const urlParms = new URLSearchParams(window.location.search);
+const msg = urlParms.get('msg');
+
+if (msg) {
+    msgBox.innerHTML = msg;
+} else {
+    //display:none
+}
