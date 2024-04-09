a7f8b10c053543731077e569c8f6721b9bdb52dd
Ahmet Uysal <ahmet-uysal@outlokk.de>
2024-04-09 16:04:49 +0200

    java script

--- assets/src/contacts/contacts.js
+++ assets/src/contacts/contacts.js
@@ -1,17 +1,35 @@
 let contacts = [];
+let firstLetter = [];
 
-function contactList() {
+async function initContacts() {
+    contacts = await getContactList(emailParameter);
+    firstLetter = getFirstLetterArray(contacts);
+    console.log(contactList);
+}
+
+function contactList(email) {
     let content = document.getElementById('content');
     content.innerHTML = '';
 
     for (let i = 0; i < contacts.length; i++) {
-        const contact = contacts[i];
+        const email = contacts[i];
         content.innerHTML += `
-        <div class="contact_card">
-        <button class="contact_button_add_new bold_21">Add new contact<img src="../../img/contacts_person_add.svg" alt="person_add"></button>
-
+        <div>
+        <img src="../../img/contacts_add-new-vector.svg" alt="partition_wall">
+        <div></div>
         </div>
         `;
+        
+    }
+}
+
+function getFirstLetterArray(array) {
+    for (let i = 0; i < array.length; i++) {
+        const letter = array[i].charAt(0);
+
+        if (!firstLetter.includes(letter)) {
+            firstLetter.push(letter);
+        }
         
     }
 }
