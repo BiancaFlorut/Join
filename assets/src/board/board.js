b6b574dcd852a7e3881458504a7b20439c53a289
Bianca Floruț <150796482+BiancaFlorut@users.noreply.github.com>
2024-04-11 21:03:37 +0200

    edit big card in bord drop down contacts with logo and checkbox no functionality

--- assets/src/board/board.js
+++ assets/src/board/board.js
@@ -1,6 +1,7 @@
 let tasks = []; 
 let user;
 const SUBTASK_CHECKBOX_PATH = '../../img/checkbox_board';
+const CHECKBOX_PATH = '../../img/checkbox';
 
 let currentDraggedElement;
 
@@ -230,12 +231,8 @@ function editTask(taskId) {
   const due_date = new Date(task.due_date);
   const valueDate = due_date.toISOString().split('T')[0];
   const minDateValue = new Date().toISOString().split('T')[0];
-
   let priorityClasses = ['', '', ''];
   priorityClasses[task.priority] = priorityClass;
-  
-  let prioritySrcIcons = [''];
-
   container.innerHTML = /*html*/`
     <div class="big_card_header" style="justify-content: flex-end">
       <div class="close_icon" onclick="closeBigCardView()">
@@ -279,8 +276,49 @@ function editTask(taskId) {
           </div>
         </div>
       </div>
+      <div>
+        <div class="big_card_edit_title">
+          <span class="big_card_edit_title_header">Assigned to</span>
+          <div class='big_card_edit_assigned_to_custom_select'>
+            <div>
+              <div class='big_card_edit_title_input'>Select contacts to assign</div>`
+                + getOptionForAssignedTo(user.contacts, task) + /*html*/`
+            </div>
+          </div>
+          <div class="df_ac">`
+            + getContactsLogoHTML(task.assign_to) + /*html*/`
+          </div>
+        </div>
+      </div>
     </div>
   `;
+}
+
+function getContactsLogoHTML(contacts) {
+  let html = '';
+  contacts.forEach( contact => html += getContactLogoForBigCardEditHTML(contact));
+  return html;
+}
+
+function getContactLogoForBigCardEditHTML(contact) {
+  return /*html*/`
+  <div class='contacts_icon big_card_edit_contact_icons' style="background-color: ${contact.color}">${getInitials(contact.name)}</div>
+  `;
+}
+
+function getOptionForAssignedTo(contacts, task) {
+  let html = '';
+  contacts.forEach( (contact, i) => {
+    let checked = '';
+    if (task.assign_to.some( assignToContact => assignToContact.email == contact.email)) {
+      checked = '_checked';
+    }
+    let logoHTML = getContactLogoForBigCardEditHTML(contact);
+    html += /*html*/`
+      <div class="df_ac big_card_edit_contacts_select">${logoHTML}<span class="flex_1">${contact.name}</span><img src="${CHECKBOX_PATH}${checked}.svg" alt="checkbox"></div >
+    `
+  });
+  return html;
 }
 
 function togglePriorityTo(taskId, priorityValue, buttonElement) {
@@ -332,11 +370,17 @@ function getAssignedToHTML(array) {
 function getContactForBigCardHTML(contact) {
   let you = contact.name == user.name ? ' (You)' : ''; 
   return /*html*/`
-    <div class='big_card_assigned_to'>
-      <div class='contacts_icon' style="background-color: ${contact.color}">${getInitials(contact.name)}</div>
+    <div class='big_card_assigned_to'>`
+      + getContactLogoHTML(contact) + /*html*/`  
       <div>${contact.name + you}</div>
     </div>
-  `
+  `;
+}
+
+function getContactLogoHTML(contact) {
+  return /*html*/`
+    <div class='contacts_icon' style="background-color: ${contact.color}">${getInitials(contact.name)}</div>
+  `;
 }
 
 function closeBigCardView(){
