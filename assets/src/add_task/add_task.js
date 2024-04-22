b4e857eeb73fdf09a0f47bb35442813267b9de12
Bianca Floruț <150796482+BiancaFlorut@users.noreply.github.com>
2024-04-22 21:22:14 +0200

    add task contacts sorted

--- assets/src/add_task/add_task.js
+++ assets/src/add_task/add_task.js
@@ -79,6 +79,7 @@ function initUserAndGenerateHTML() {
 
 function getOptionForAssignedToCreateTask(contacts, task, exceptUserEmail) {
   let html = "";
+  contacts.sort((a, b) => a.name.localeCompare(b.name));
   contacts.forEach((contact) => {
     if (contact.email != exceptUserEmail){
       let checked = "";
