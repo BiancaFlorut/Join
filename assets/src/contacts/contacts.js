4d36730fe3c6c4537c0ebf40791ee5d9f3febfac
Ahmet Uysal <ahmet-uysal@outlook.de>
2024-04-23 13:53:10 +0200

    responsive

--- assets/src/contacts/contacts.js
+++ assets/src/contacts/contacts.js
@@ -35,9 +35,7 @@ function contactListHTML() {
         content.innerHTML += /*html*/`
             <div>
                 <div class="contact_letter">${letter}</div>
-                <div>
-                    <img src="../../img/contacts_add-new-vector.svg" alt="partition_wall">
-                </div>   `;
+                 `;
         contactsFirstLetter.forEach(contact => {
             content.innerHTML += /*html*/`
                 <button onclick="showContactDetails(this, '${contact.email}')" class="contact_name">
@@ -132,6 +130,12 @@ async function addNewContact() {
     addClose();
     getElementWithId('createContactButton').disabled = false;
     initContacts();
+}
+
+function createContact() {
+    document.getElementById('overlyContact').style.display='flex';
+    document.getElementById('contactSucces').style.display='flex';
+    document.getElementById('overlayAddContact').style.display='none';
 }
 
 async function deleteContact(email) {
